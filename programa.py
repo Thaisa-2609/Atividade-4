b338ad67c213401836fc4ebbbd73dde6dc8fa12c
Thaisa-2609 <thaisasdias@gmail.com>
2024-12-02 14:45:18 -0300

    adicionei a primeira parte do códico do meu programa

---
+++ programa.py
@@ -0,0 +1,10 @@
+contatos = {}
+
+while True:
+    print("\n=== Cadastro de Contatos ===")
+    print("1. Adicionar contato")
+    print("2. Listar contatos")
+    print("3. Remover contato")
+    print("4. Sair")
+    
+    
